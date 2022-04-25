6635206e702406dccfb0e108322ea57c9a1575e8
Leszek Michno <leszek.michno@gmail.com>
2022-04-25 18:01:19 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const data = [
+  { id: 1, title: "Wiadomość nr 1", body: "Zawartośc wiadomości nr 1..." },
+  { id: 2, title: "Wiadomość nr 2", body: "Zawartośc wiadomości nr 2..." },
+  { id: 3, title: "Wiadomość nr 3", body: "Zawartośc wiadomości nr 3..." },
+];
+
+setInterval(() => {
+  const index = data.length + 1;
+  data.push({
+    id: index,
+    title: `Wiadomość nr ${index}`,
+    body: `Zawartośc wiadomości nr ${index}...`,
+  });
+}, 5000);
+
+const App = () => {
+  const [comments, setComments] = useState([...data]);
+
+  const getData = () => {
+    if (comments.length !== data.length) {
+      setComments([...data]);
+    }
+  };
+  useEffect(() => {
+    setInterval(getData, 2000);
+  }, [getData]);
+
+  const com = comments.map((comment) => (
+    <div key={comment.id}>
+      <h4>{comment.title}</h4>
+      <div>{comment.body}</div>
     </div>
-  );
-}
+  ));
+
+  return <div>{com.reverse()}</div>;
+};
 
 export default App;
---
+++ src/Test.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Test = () => {
+    return (
+        <div>
+            Działa!!!
+        </div>
+      );
+}
+ 
+export default Test;
